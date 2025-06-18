818fe112608abc7dda3854bb7b0848ad56a5272a
APEL-research <satish.belkhode.iitr@gmail.com>
2025-06-18 11:25:33 +0000

    Deploying to gh-pages from @ APEL-research/APEL-research.github.io@d782b6a7aaf1b973b6cb82b480b16b88ed5b7ab7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,20 +3,13 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-home",
-    title: "Home",
+    id: "nav-about",
+    title: "About",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-home",
-          title: "Home",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/";
-          },
-        },{id: "nav-blog",
+  },{id: "nav-blog",
           title: "blog",
           description: "",
           section: "Navigation",
